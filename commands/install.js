b15b00d1d0f76a5dac312c5840f5142a7862adc5
Chris Saylor <cjsaylor@gmail.com>
2014-12-20 20:14:03 -0500

    Glob added to install command.

--- commands/install.js
+++ commands/install.js
@@ -1,18 +1,20 @@
 var path = require('path');
 var fs = require('fs');
+var glob = require('glob');
 module.exports = function() {
     var config = require('../badassets.json');
     var manifest = config.files;
     config.paths.forEach(function(entry) {
-        var dir = path.normalize(process.cwd() + '/' + entry);
+        var cwd = process.cwd();
         // replace with glob
-        fs.readdir(dir, function(err, files) {
+        glob(entry, {nodir: true}, function(err, files) {
+            if (err) {
+                console.error(err);
+            }
             files.forEach(function(file) {
-                var normalized = path.normalize(entry + '/' + file);
-                console.log(normalized);
+                console.log(path.relative(process.cwd(), file));
                 // push to manifest with hash and versioning
             });
         });
     });
-    console.log(manifest);
 };
