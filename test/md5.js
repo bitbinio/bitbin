a65a8d76781d52ba391f4d4137930df0d98bb9d6
Chris Saylor <cjsaylor@gmail.com>
2014-12-22 09:53:57 -0500

    Started tests with mocha.

---
+++ test/md5.js
@@ -0,0 +1,11 @@
+var md5 = require(__dirname + '/../src/md5')();
+
+describe('md5', function() {
+    describe('#computeFromFile', function() {
+        it('should promise an md5 hash', function(done) {
+            var file = __dirname + '/fixture/md5test.txt';
+            var md5hash = '77284ae4aac90cd005586850dce5fbd9';
+            md5.computeFromFile(file).should.eventually.equal(md5hash).notify(done);
+        });
+    });
+});
