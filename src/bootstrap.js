7c3462d6ddf8cfcdb7dc7a93d8de734f388c4336
Chris Saylor <cjsaylor@gmail.com>
2014-12-23 21:03:05 -0500

    Fixed spacing.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,8 +3,8 @@ var bottle = require('bottlejs')();
 module.exports = function() {
     bottle.register(require(__dirname + '/config'));
     bottle.register(require(__dirname + '/manifest'));
-	bottle.register(require(__dirname + '/md5'));
-	bottle.register(require(__dirname + '/commands/publish'));
+    bottle.register(require(__dirname + '/md5'));
+    bottle.register(require(__dirname + '/commands/publish'));
 
-	return bottle.container;
+    return bottle.container;
 };
