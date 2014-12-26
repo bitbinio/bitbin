0112b956553ffb1986a9a971717e847ba0ac4869
Chris Saylor <cjsaylor@gmail.com>
2014-12-26 18:02:46 -0500

    Implemented local adapter filterExisting.

--- src/adapters/local.js
+++ src/adapters/local.js
@@ -1,9 +1,37 @@
 var util = require('util');
+var Q = require('q');
+var junk = require('junk');
 var BaseAdapter = require(__dirname + '/../base_adapter');
 
-var LocalAdapter = function(fs) {
+var LocalAdapter = function(config, fs, glob, md5) {
+    this.uploadPath = config.retrieve().options.uploadPath;
     this.fs = fs;
-}
+    this.glob = glob;
+    this.md5 = md5;
+};
+
+/**
+ * @todo abstract this.
+ */
+var filterJunk = function(files) {
+    return files.filter(junk.not);
+};
+
+var transposeMd5 = function(files) {
+    var sumPromises = [];
+    files.forEach(function(file) {
+        sumPromises.push(this.md5.computeFromFile(file));
+    }.bind(this));
+    return Q.all(sumPromises)
+        .then(function(sums) {
+            return files.map(function(entry, i) {
+                return {
+                    name: entry,
+                    hash: sums[i]
+                };
+            });
+        });
+};
 
 util.inherits(LocalAdapter, BaseAdapter);
 
@@ -12,12 +40,26 @@ util.inherits(LocalAdapter, BaseAdapter);
  *
  * @param array files
  * @return array
- * @todo implement It is just passing through for now.
  */
 LocalAdapter.prototype.filterExisting = function(files) {
-    return files;
+    var uploadPath = this.uploadPath;
+    return Q.nfcall(this.glob, uploadPath + '/**/*', {nodir: true})
+        .then(filterJunk)
+        .then(transposeMd5.bind(this))
+        .then(function(entries) {
+            return files.filter(function(file) {
+                return !entries.some(function(entry) {
+                    return entry.name.substr(uploadPath.length + 1) === file.name && entry.hash === file.hash;
+                });
+            });
+        });
 };
 
 module.exports = function(container) {
-    return new LocalAdapter(container.node.fs);
+    return new LocalAdapter(
+        container.config,
+        container.node.fs,
+        container.glob,
+        container.md5
+    );
 };
