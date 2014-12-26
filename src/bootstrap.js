5ff1286adecf09643bf36b2774374f91dcd3e9cc
Chris Saylor <cjsaylor@gmail.com>
2014-12-25 20:57:45 -0500

    Bootstrap adapter injector.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,13 +1,16 @@
-var bottle = require('bottlejs')();
+var bottle = require('bottlejs').pop('main');
 
 module.exports = function() {
     bottle.factory('node_fs', function() { return require('fs') });
 
+    bottle.register(require(__dirname + '/adapter_injector'));
     bottle.register(require(__dirname + '/config'));
     bottle.register(require(__dirname + '/manifest'));
     bottle.register(require(__dirname + '/md5'));
     bottle.register(require(__dirname + '/commands/init'));
     bottle.register(require(__dirname + '/commands/publish'));
+
+    bottle.container.adapter_injector.inject();
 
     return bottle.container;
 };
