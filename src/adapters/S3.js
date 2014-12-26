b890da27f53704456d389546c75796136115b1c3
Chris Saylor <cjsaylor@gmail.com>
2014-12-26 12:03:36 -0500

    Update S3 adapter to inherit the base adapter.

--- src/adapters/S3.js
+++ src/adapters/S3.js
@@ -1,5 +1,12 @@
+var util = require('util');
+var BaseAdapter = require(__dirname + '/../base_adapter');
+
+var S3Adapter = function(fs) {
+    this.fs = fs;
+}
+
+util.inherits(S3Adapter, BaseAdapter);
+
 module.exports = function() {
-    return function() {
-        console.log('S3 adapter not implemented yet.');
-    };
+    return new S3Adapter();
 };
