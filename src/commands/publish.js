910c0dee1875752a3e6d3a6e2ca94469e9d2f45f
Chris Saylor <cjsaylor@gmail.com>
2014-12-26 12:04:24 -0500

    Added adapter to the publish command dependencies.

--- src/commands/publish.js
+++ src/commands/publish.js
@@ -1,11 +1,13 @@
-var Publish = function(manifest) {
+var Publish = function(manifest, adapter) {
     this.manifest = manifest;
+    this.adapter = adapter;
 };
 
 Publish.prototype.handle = function() {
     this.manifest.localFiles()
         .then(this.manifest.transposeWithMD5.bind(this.manifest))
         .then(this.manifest.filterInManifest.bind(this.manifest))
+
         // @todo filter out files already existing in upload adapter
         // @todo upload all remaining files
         // @todo update filenames with versions
@@ -30,4 +32,4 @@ Publish.prototype.handle = function() {
 module.exports = Publish;
 module.exports.$name = 'command.publish';
 module.exports.$type = 'service';
-module.exports.$inject = ['manifest'];
+module.exports.$inject = ['manifest', 'adapter'];
