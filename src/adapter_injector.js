0731f8629a52ef0c2c9bf12ef7ad049e793cf095
Chris Saylor <cjsaylor@gmail.com>
2014-12-25 20:53:52 -0500

    Added adapter injector module.

---
+++ src/adapter_injector.js
@@ -0,0 +1,33 @@
+// @todo adapt this to a file checker on an adapter folder.
+var builtIn = ['S3'];
+
+var AdapterInjector = function(config) {
+    this.config = config;
+};
+
+AdapterInjector.prototype.inject = function() {
+    var adapter = this.config.retrieve().adapter;
+    var bottle = require('bottlejs').pop('main');
+    var adapterImpl;
+    var path = builtIn.indexOf(adapter) >= 0 ?
+        __dirname + '/adapters/' + adapter :
+        adapter;
+    if (!adapter) {
+        console.error('No upload adapter defined.');
+        process.exit(1);
+    }
+    try {
+        adapterImpl = require(path);
+        bottle.factory('adapter', adapterImpl);
+    } catch(e) {
+        bottle.factory('adapter', function() {
+            return function() {
+                console.error('Invalid adapter [%s]', path);
+            };
+        });
+    }
+};
+
+module.exports = AdapterInjector;
+module.exports.$name = 'adapter_injector';
+module.exports.$inject = ['config'];
