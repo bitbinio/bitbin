e5298329615392583697fa1d12ec231d4c82a008
Chris Saylor <cjsaylor@gmail.com>
2014-12-27 23:15:19 -0500

    Implemented version attaching/updating in base adapter.

--- src/base_adapter.js
+++ src/base_adapter.js
@@ -1,4 +1,9 @@
-var BaseAdapter = function() {};
+var path = require('path');
+var BaseAdapter = function() {
+    this.patterns = {
+        version: /^(.*)__v(\d+).*$/
+    };
+};
 
 /**
  * Filter out of the files argument array that which is already existing.
@@ -11,6 +16,27 @@ var BaseAdapter = function() {};
  */
 BaseAdapter.prototype.filterExisting = function(files) {
     throw new Error('filterExisting not implemented on this adapter.');
+};
+
+var versionReplacer = function(match, base, version) {
+    return base + '__v' + (parseInt(version) + 1);
+};
+
+/**
+ * Attach/update the version of provided file.
+ *
+ * The default behavior is to ammend __v# prior to the extension.
+ * If no extension exists, __v# will be appended to the end of the filename.
+ *
+ * @param string file
+ * @return string
+ */
+BaseAdapter.prototype.attachVersion = function(filename) {
+    var extension = path.extname(filename);
+    var baseName = path.basename(filename, extension);
+    return this.patterns.version.test(baseName) ?
+        filename.replace(this.patterns.version, versionReplacer) + extension :
+        baseName + '__v1' + extension;
 };
 
 module.exports = BaseAdapter;
---
+++ test/base_adapter.js
@@ -0,0 +1,25 @@
+var BaseAdapter = require(__dirname + '/../src/base_adapter');
+
+describe('base_adapter', function() {
+    describe('#attachVersion', function() {
+        var adapter = new BaseAdapter();
+        var expectations = [
+            ['filename.jpg',      'filename__v1.jpg'],
+            ['filename',          'filename__v1'],
+            ['filename__v1',      'filename__v2'],
+            ['filename__v1.jpg',  'filename__v2.jpg'],
+            ['filename__v9.jpg',  'filename__v10.jpg'],
+            ['filename__v9',      'filename__v10'],
+            ['filename__v10.jpg', 'filename__v11.jpg'],
+            ['filename__v10',     'filename__v11'],
+            ['file.name.jpg',     'file.name__v1.jpg'],
+            ['file.name__v1.jpg', 'file.name__v2.jpg']
+        ];
+        expectations.forEach(function(expectation, i) {
+            it('should attach/update appropriate version information to the filename #' + i, function() {
+                expect(adapter.attachVersion(expectation[0])).to.equal(expectation[1]);
+            });
+        });
+        
+    });
+});
