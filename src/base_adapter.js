fa8d63f000caf47778c2ce8ead43d1988c85d14a
Chris Saylor <cjsaylor@gmail.com>
2014-12-27 21:09:03 -0500

    Updated docblock for base adapter filterExisting.

--- src/base_adapter.js
+++ src/base_adapter.js
@@ -1,5 +1,14 @@
 var BaseAdapter = function() {};
 
+/**
+ * Filter out of the files argument array that which is already existing.
+ *
+ * Depending on what the operation does, this can either return an array
+ * (of similar format) or a promise.
+ *
+ * @param array files
+ * @return array|promise
+ */
 BaseAdapter.prototype.filterExisting = function(files) {
     throw new Error('filterExisting not implemented on this adapter.');
 };
